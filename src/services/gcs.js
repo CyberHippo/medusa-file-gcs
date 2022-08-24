9b37b1ae77fc330c614e31ea7bd7b781fa6567e5
Hippolyte Vergnol <hippolyte@vergnol.fr>
2022-08-24 13:29:07 +0200

    feat: lint using latest eslint version (#27)

--- src/services/gcs.js
+++ src/services/gcs.js
@@ -1,51 +1,52 @@
-import { Storage } from "@google-cloud/storage"
-import { FileService } from "medusa-interfaces"
+/* eslint-disable no-empty-pattern */
+import { Storage } from '@google-cloud/storage';
+import { FileService } from 'medusa-interfaces';
 
 class GCSService extends FileService {
   constructor({ }, options) {
-    super()
-    this.bucketName = options.bucketName
-    this.keyFileName = options.keyFileName
+    super();
+    this.bucketName = options.bucketName;
+    this.keyFileName = options.keyFileName;
   }
 
   upload(file) {
     const storage = new Storage({
       keyFilename: this.keyFileName,
-    })
+    });
 
-    const bucket = storage.bucket(this.bucketName)
+    const bucket = storage.bucket(this.bucketName);
 
     return new Promise((resolve, reject) => {
       bucket.upload(file.path, (err, newfile) => {
         if (err) {
-          reject(err)
-          return
+          reject(err);
+          return;
         }
 
-        resolve({ url: newfile.publicUrl()})
-      })
-    })
+        resolve({ url: newfile.publicUrl() });
+      });
+    });
   }
 
   delete(file) {
     const storage = new Storage({
       keyFilename: this.keyFileName,
-    })
+    });
 
-    const bucket = storage.bucket(this.bucketName)
+    const bucket = storage.bucket(this.bucketName);
 
-    const fileToDelete = bucket.file(`${file}`)
+    const fileToDelete = bucket.file(`${file}`);
 
     return new Promise((resolve, reject) => {
       fileToDelete.delete((err, apiResponse) => {
         if (err) {
-          reject(err)
-          return
+          reject(err);
+          return;
         }
-        resolve(apiResponse)
-      })
-    })
+        resolve(apiResponse);
+      });
+    });
   }
 }
 
-export default GCSService
+export default GCSService;
